a970b129fc0c5c68202c8faa7ad3b5e528de425d
NikolaiUrne <156089559+NikolaiUrne@users.noreply.github.com>
2024-03-06 09:38:58 +0100

    hei

---
+++ DeveloperFolder/tc_helpers.py
@@ -0,0 +1,22 @@
+"""This module contains helper functions for the PLC translator."""
+
+
+class Tcdut:
+    """Represents a DUT."""
+
+    def __init__(self, name, code):
+        """Initialize the DUT object with a name and code."""
+        self.name = name
+        self.code = code
+
+    def header(self):
+        """Generate the header for the DUT."""
+        return f"""<?xml version="1.0" encoding="utf-8"?>
+<TcPlcObject Version="1.1.0.1" ProductVersion="3.1.4024.12">
+  <DUT Name= "{self.name}" Id="{{572155cd-1cb7-4296-b8e0-698682541d76}}">
+    <Declaration><![CDATA["""
+
+    footer = """
+]]></Declaration>
+  </DUT>
+</TcPlcObject>"""
