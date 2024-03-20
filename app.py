e4830c13883ac10d2f4a211eb297a954f3550c9f
Nikolai Urne <156089559+NikolaiUrne@users.noreply.github.com>
2024-03-20 11:31:41 +0000

    Refactor code for TIA to TwinCAT converter

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 import logging
 import logging.config
+import os
+from pathlib import Path
 from tkinter import Text, filedialog
 
 import customtkinter as ctk  # type : ignore
@@ -11,7 +13,7 @@ import src.plctranslator.tia_translator as tia_translator  # noqa: PLR0402
 from config.config import get_config
 
 _CONFIG = get_config()
-_LOGGER = logging.getLogger(__name__)
+
 if _CONFIG.logging is not None:
     logging.config.dictConfig(_CONFIG.logging)
 
@@ -40,7 +42,7 @@ def choose_sourcefile():
         else:
             textbox.delete("1.0", "end")
             status_label.configure(text="Convertion is not possible", fg_color="red")  # Endret fra config til configure
-            konverter_btn.configure(state="disabled")
+            converting_btn.configure(state="disabled")
             textbox.insert("1.0", tia_translator.log_stream.getvalue())
             tia_translator.log_stream.truncate()
             tia_translator.log_stream.seek(0)
@@ -63,12 +65,15 @@ def check_convertion():
     is_possible = (
         (sourcefile != "") and (destinationfolder != "") and tia_translator.check(tia_helpers.read_scl_file(sourcefile))
     )
-
+    tia_translator.log_stream.truncate()
+    tia_translator.log_stream.seek(0)
     if is_possible:
-        konverter_btn.configure(state="normal")
+        converting_btn.configure(state="normal")
+        open_file_explorer_button.configure(state="normal")
+        show_full_info_button.configure(state="normal")
 
 
-def konverter():
+def converter():
     """Converts the source file to the target folder."""
     fulltext = tia_helpers.read_scl_file(sourcefile_var.get())
     tia_translator.translate(fulltext, destinationfolder_var.get())
@@ -78,42 +83,122 @@ def konverter():
     tia_translator.log_stream.truncate()
     tia_translator.log_stream.seek(0)
     status_label.configure(text="Convertion successful", fg_color="magenta")
-    konverter_btn.configure(state="disabled")
+    converting_btn.configure(state="disabled")
+
+
+def open_file_explorer_destenation_folder():
+    """Opens the file explorer to the destination folder."""
+    sourcefile = sourcefile_var.get()
+    destinationfolder = destinationfolder_var.get()
+    is_possible = (
+        (sourcefile != "") and (destinationfolder != "") and tia_translator.check(tia_helpers.read_scl_file(sourcefile))
+    )
+    if is_possible:
+        os.startfile(destinationfolder_var.get())  # noqa: S606
+
+
+def show_full_info_from_converting():
+    """Shows the full info from the converting in the textbox."""
+    fulltext = tia_helpers.read_scl_file(sourcefile_var.get())
+    sourcefile = sourcefile_var.get()
+    destinationfolder = destinationfolder_var.get()
+    is_possible = (
+        (sourcefile != "") and (destinationfolder != "") and tia_translator.check(tia_helpers.read_scl_file(sourcefile))
+    )
+    if is_possible:
+        textbox.delete("1.0", "end")
+        textbox.insert("1.0", fulltext)
+    print(fulltext)
+    tia_translator.log_stream.truncate()
 
 
 root = ctk.CTk()
-root.title("Tia Portal to TwinCAT converter")
-root.geometry("850x350")  # Oppdatert for ekstra plass
-root.resizable(width=False, height=False)
+root.title("Goodtech TIA to TwinCAT Translator")
+script_dir = Path(__file__).parent  # Henter mappen hvor scriptet ligger
+icon_path = "final_gt_ico.ico"
+try:
+    root.iconbitmap(icon_path)
+except Exception as e:
+    print(f"Feil ved setting av ikon: {e}")
+
+root.geometry("850x500")  # Oppdatert for ekstra plass
+root.resizable(width=True, height=True)
+root.minsize(850, 500)
+
 
 # Opprette rammer for layout-separasjon
-left_frame = ctk.CTkFrame(master=root)
-left_frame.pack(side=ctk.LEFT, fill=ctk.BOTH, expand=True)
+left_top_frame = ctk.CTkFrame(master=root)
+left_top_frame.pack(side=ctk.LEFT, fill=ctk.BOTH, expand=True)
+
+
+left_btm_f = ctk.CTkFrame(master=left_top_frame)
+left_btm_f.pack(side=ctk.BOTTOM, fill=ctk.BOTH, expand=True)
+
+left_btm_f_l = ctk.CTkFrame(master=left_btm_f)
+left_btm_f_l.pack(side=ctk.LEFT, fill=ctk.BOTH, expand=True)
+left_btm_f_r = ctk.CTkFrame(master=left_btm_f)
+left_btm_f_r.pack(side=ctk.RIGHT, fill=ctk.BOTH, expand=True)
+
 
 right_frame = ctk.CTkFrame(master=root)
 right_frame.pack(side=ctk.RIGHT, fill=ctk.BOTH, expand=True)
 
+
 sourcefile_var = ctk.StringVar()
 destinationfolder_var = ctk.StringVar()
 
+
+"""Left frame"""
 # sourcefile part
-ctk.CTkLabel(left_frame, text="Choose sourcefile:").pack(pady=(10, 0), padx=20)
-ctk.CTkEntry(left_frame, textvariable=sourcefile_var, state="readonly").pack()
-ctk.CTkButton(left_frame, text="Browse", command=choose_sourcefile).pack()
+ctk.CTkLabel(left_top_frame, text="Choose sourcefile:").pack(pady=10)
+ctk.CTkEntry(left_top_frame, textvariable=sourcefile_var, state="readonly").pack()
+ctk.CTkButton(left_top_frame, text="Browse", command=choose_sourcefile).pack()
 
 # destination folder part
-ctk.CTkLabel(left_frame, text="Choose destination folder:").pack(padx=20)
-ctk.CTkEntry(left_frame, textvariable=destinationfolder_var, state="readonly").pack()
-ctk.CTkButton(left_frame, text="Browse", command=choose_destinationfolder).pack(padx=30)
+ctk.CTkLabel(left_top_frame, text="Choose destination folder:").pack(padx=10)
+ctk.CTkEntry(left_top_frame, textvariable=destinationfolder_var, state="readonly").pack()
+ctk.CTkButton(left_top_frame, text="Browse", command=choose_destinationfolder).pack(padx=20)
+
+
+# Statuslabel for converting, if possible or not
+status_label = ctk.CTkLabel(left_top_frame, text="Waiting For Input", fg_color="gray")
+status_label.pack(fill=ctk.X, pady=40, padx=5)
+
+# Convert button
+converting_btn = ctk.CTkButton(left_top_frame, text="Convert", command=converter, state="disabled")
+converting_btn.pack(pady=3)
+
+
+# Open file explorer button
+open_file_explorer_button = ctk.CTkButton(
+    left_btm_f_l, text="Open destination folder", command=open_file_explorer_destenation_folder, state="disabled"
+)
+open_file_explorer_button.pack(pady=25)
+
+
+# Show full info from converting
+show_full_info_button = ctk.CTkButton(
+    left_btm_f_r, text="Show convertion result", command=show_full_info_from_converting, state="disabled"
+)
+show_full_info_button.pack(pady=25)
+
+# clear button
+textbox: Text
+clear_button = ctk.CTkButton(left_btm_f_l, text="Clear", command=lambda: textbox.delete("1.0", "end"))
+clear_button.pack()
+
+# Exit button
+exit_button = ctk.CTkButton(left_btm_f_r, text="Exit", command=root.quit)
+exit_button.pack()
+
+
+"""Right frame"""
+# Label for right_frame
+ctk.CTkLabel(right_frame, text="Output window for checks:").pack(pady=10)
 
 # Textboks in right_frame
 textbox = Text(right_frame, height=20, width=50)
-textbox.pack(fill=ctk.BOTH, expand=True, padx=20, pady=20)
+textbox.pack(fill=ctk.BOTH, expand=True, padx=(0, 10), pady=(0, 10))
 
-status_label = ctk.CTkLabel(left_frame, text="Waiting For Input", fg_color="gray", padx=20)
-status_label.pack(fill=ctk.X, pady=15)
-
-konverter_btn = ctk.CTkButton(left_frame, text="Convert", command=konverter, state="disabled")
-konverter_btn.pack()
 
 root.mainloop()
